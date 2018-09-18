b99a3c226af092d77c0fa353903036b6ac1ccbe9
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2018-09-18 18:17:14 +0300

    added a list of API's that should be implemented

--- index.js
+++ index.js
@@ -3,5 +3,19 @@ console.log("Loading fake fs");
 module.exports = {
     stat:"global.functionUndefined",
     writeFile:global.functionUndefined,
-    readFile:global.functionUndefined
+    readFile:global.functionUndefined,
+    createWriteStream:functionUndefined,
+    createReadStream:functionUndefined,
+    open:functionUndefined,
+    close:functionUndefined,
+    fstat:functionUndefined,
+    readDir:functionUndefined,
+    statSync:functionUndefined,
+    writeFileSync:functionUndefined,
+    readFileSync:functionUndefined,
+    existsSync:functionUndefined,
+    mkdirSync:functionUndefined,
+    unlinkSync:functionUndefined,
+    rmdirSync:functionUndefined,
+
 };
