0ff7ceb7db54e2c73278b6f113d4221a12603690
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2018-10-22 18:04:47 +0300

    web streams working prototype done

--- tests/browserTests/sampleTest.js
+++ tests/browserTests/sampleTest.js
@@ -1,23 +1,54 @@
 const fs = require("fs");
 
-fs.appendFile("/indexed/text.txt", "Salut din IndexedDB\n", function (err,response) {
+/*fs.appendFile("/indexed/text.txt", "Salut din IndexedDB\n", function (err,response) {
     fs.readFile("/indexed/text.txt",function(err, contents){
         console.log(contents.toString());
     })
-});
+});*/
 
 setTimeout(function(){
-    const fs = require("fs");
+
     var readStream = fs.createReadStream("/indexed/text.txt");
-    console.log(readStream);
     readStream
-        .on('data', function (err, chunk) {
+        .on('data', function (chunk) {
             console.log(chunk.toString());
         })
         .on('end', function () {
-            console.log("end");
+            console.log("END");
+        }).on('end',function(){
+            console.log("end2")
+    });
+},2000);
+
+
+    var readStream = fs.createReadStream("/indexed/text.txt");
+    readStream
+        .on('data', function (chunk) {
+            console.log(chunk.toString());
+        })
+        .on('end', function () {
+            console.log("END");
         });
-},1200);
+
+
+setTimeout(function(){
+    const fs2 = require("fs");
+    var readStream = fs2.createReadStream("/indexed/text.txt");
+
+    setTimeout(function(){
+        readStream
+            .on('data', function (chunk) {
+                console.log(chunk.toString());
+            })
+    },1000);
+    readStream
+        .on('end', function () {
+            console.log("END");
+        }).on('end',function(){
+        console.log("end2")
+    });
+},1400);
+
 
 
 
