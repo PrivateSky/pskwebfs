a023277289ed93299b503f959457906c20b41a24
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2018-10-23 17:47:01 +0300

    wip

--- tests/browserTests/sampleTest.js
+++ tests/browserTests/sampleTest.js
@@ -1,4 +1,10 @@
 const fs = require("fs");
+const assert = require("assert");
+
+
+/*fs.readFile("/indexed/text.txt",function(err, contents){
+    console.log(contents.toString());
+})*/
 
 /*fs.appendFile("/indexed/text.txt", "Salut din IndexedDB\n", function (err,response) {
     fs.readFile("/indexed/text.txt",function(err, contents){
@@ -6,40 +12,68 @@ const fs = require("fs");
     })
 });*/
 
-setTimeout(function(){
 
-    var readStream = fs.createReadStream("/indexed/text.txt");
-    readStream
-        .on('data', function (chunk) {
-            console.log(chunk.toString());
-        })
-        .on('end', function () {
-            console.log("END");
-        }).on('end',function(){
-            console.log("end2")
+
+/*var readStream = fs.createReadStream("/isndexed/text.txt");
+readStream
+    .on('data', function (chunk) {
+        console.log(chunk.toString());
+    })
+    .on('end', function () {
+        console.log("END");
+    }).on('error',function(err){
+        console.log("On error",err);
+});
+*/
+
+/*var readStream = fs.createReadStream("/indexed/text.txt");
+readStream
+    .on('data', function (chunk) {
+        console.log(chunk.toString());
+    })
+    .on('end', function () {
+        console.log("END");
     });
-},2000);
 
 
-    var readStream = fs.createReadStream("/indexed/text.txt");
-    readStream
-        .on('data', function (chunk) {
-            console.log(chunk.toString());
-        })
-        .on('end', function () {
-            console.log("END");
-        });
 
 
-setTimeout(function(){
+fs.open("/indexed/text.txt", "r+", function (error, fd) {
+
+    var contentOfValidFile = "TralalaRafaelR"
+    if (error) throw error;
+    var buffer = new Buffer(14);
+    fs.read(fd, buffer, 0, buffer.length, null, function (error, bytesRead, buffer) {
+        assert.equal(error, null, 'TEST FAILED: err not null');
+        assert.equal(bytesRead, 14, 'TEST FAILED - READ - no of bytes');
+        var data = buffer.toString("utf8");
+        assert.equal(data, contentOfValidFile, 'TEST FAILED: read');
+    });
+
+    fs.close(fd, function (err) { });
+});
+*/
+
+
+
+var writeStream = fs.createWriteStream("/indexed/text.txt");
+writeStream.write("Tralala");
+writeStream.on('finish', () => {
+    console.log('wrote all data to file');
+});
+writeStream.end("Mastaleru");
+writeStream.write("Rafael");
+
+/*setTimeout(function(){
     const fs2 = require("fs");
     var readStream = fs2.createReadStream("/indexed/text.txt");
 
     setTimeout(function(){
-        readStream
+        var s = readStream
             .on('data', function (chunk) {
                 console.log(chunk.toString());
-            })
+            });
+        console.log(s);
     },1000);
     readStream
         .on('end', function () {
@@ -47,7 +81,7 @@ setTimeout(function(){
         }).on('end',function(){
         console.log("end2")
     });
-},1400);
+},1400);*/
 
 
 
